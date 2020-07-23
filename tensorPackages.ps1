9479a5266936d4362c03bf9e3fefbeaabfa6ee8f
Kaleb Byrum <33094708+kabyru@users.noreply.github.com>
2020-07-23 11:48:59 -0400

    Add files via upload

---
+++ tensorPackages.ps1
@@ -0,0 +1,15 @@
+pip install Cython
+pip install contextlib2
+pip install pillow
+pip install lxml
+pip install jupyter
+pip install matplotlib
+pip install tf_slim
+
+pip install tensorflow
+
+#These may not be needed, but just in case...
+pip install numpy
+pip install opencv-python
+pip install imutils
+pip install pyyaml
