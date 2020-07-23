6c619f818354fcd03253a4fcd017704eabbc65e3
Kaleb Byrum <33094708+kabyru@users.noreply.github.com>
2020-07-23 11:50:49 -0400

    Merging OS Release with Updated Private Release Updates

---
+++ logo.py
@@ -0,0 +1,30 @@
+def printLogo():
+    logo = """
+                       ``      `,                          
+                      'mA`    `0O-                         
+              `?hc`   `pR"    ~@q     `3+                  
+              `"XR]`   lHn    zB7    ,0&\                  
+         `^`    |GBC`  \&O`  `FX-   \Am>                   
+        ,FHXa^`  ,SBC```8B?` "RC  `+&0:`   -2A]`           
+         ,lPBMq"` `]Hp: iBT` uB+ `v@n`  `>SPn_`            
+           `+0DBAv,`$BP^,OX:`GG``2@z`` >kAz,               
+    `agnc|`   ~IWHWu'IMI`"}``+"`tm1``tkA+`                 
+    -xFXRHB@P8z+-"dMZ:`          ``?AI~`                   
+       `\}3qPMHHMO%v`              ``                      
+            ``"lCAA~                 ?AAAAAAAAAAAAAg>      
+   }XXXXXXXXmXXXmX2                  +RHHHHHHHHHHHHHv      
+   >d$d$d$$$$$$$%Ox`                 }BHHHHHHHHHHHHHv      
+        ```\}30GMH@>               ``pHHHHHHRWAnls??-      
+     `>cnFXRHHHB&PXO'              0BHHHHHHHHHHMO       
+    ~mBHHHDmhxs+uXBHW3^          '5WHHHHHRMBHHHHHHRt       
+    `3P8z+-``"dMHHBO@HDAnlt""ilnADHHHHHHHBXqenAXRHn`       
+         `~IWHHHWI"FHHB@HHHHHHHHHHHHHQmBHHHHRA1``'`        
+        "%DHHBPl,_PHHBnhHHHW&HHH&WHHHHOlABHHHHHA'          
+        |gBD0+``"XHHR5^WHHHCTHHHQcGHHHHX$MHBA>           
+         `+~```i@HHBv`lHHHR+?BHHHI^AHHHHP~`^e?` Raytheon Missiles and Defense           
+              sQHHMl `gHHHA``GHHHO,`aRHHBF-`  Circuit Card Assembly Identifier  
+              `"ah}``|MHHHa  IHHHBt``lPS"`   v 0.1 - Alpha, configured for 3 sample CCAs
+                     "FGXm|  "XXGFi` ``    Written in 2020, by Kaleb Byrum, LSIC Louisville
+                                                           
+    """
+    print(logo)
